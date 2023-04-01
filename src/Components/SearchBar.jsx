84ba76ba1de18ee0f16f3caea9e9192937cf1031
Francisco Yorlano <francisco.yorlano.arg@gmail.com>
2023-04-01 19:30:41 -0300

    nav

--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -7,8 +7,8 @@ const SearchBar = () => {
     return (
         <form>
             <input
-                type="text"
-                placeholder="Search Card"
+                type="number"
+                placeholder="Card id"
                 className="w-32 z-0 inline-block rounded-l-full text-sm leading-6 font-semibold text-slate-900 leading-6 px-2 shadow-lg hover:shadow-pink-500/50"
             />
             <button className="inline-block rounded-r-full  bg-gradient-to-r from-purple-500 to-pink-500 shadow-lg hover:shadow-pink-500/50">
