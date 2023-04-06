569c185b7e03e2b90031b9cf298e1afbd006b092
Francisco Yorlano <francisco.yorlano.arg@gmail.com>
2023-04-06 14:12:22 -0300

    card

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -9,43 +9,63 @@ import {
 } from "react-icons/ai";
 import { GiBackPain, GiAnatomy } from "react-icons/gi";
 
+// Hooks
+import { useState } from "react";
+
 // React Router
 import { Link } from "react-router-dom";
 
 const Card = (props) => {
     const { id, name, species, gender, image } = props.char;
 
+    // States
+    const [isFav, setIsFav] = useState(false);
+
+    // Handlers
+    const handleFavorite = () => {
+        setIsFav((prevIsFav) => !prevIsFav);
+    };
+
+    const handleClose = () => {};
+
     return (
-        <div class="relative w-48 bg-gradient-to-r from-purple-500 to-pink-500 border border-gray-200 rounded-lg m-none text-center text-slate-200">
-            <div class="absolute top-0 left-0 z-10 w-full h-full flex justify-center items-center">
-                <div class="flex items-center text-sm leading-6 bg-gradient-to-r from-purple-500 to-pink-500 text-white px-4 rounded-bl-md rounded-tr-md">
+        <div className="relative w-48 bg-gradient-to-r from-purple-500 to-pink-500 border border-gray-200 rounded-lg m-none text-center text-slate-200">
+            <div className="absolute top-0 left-0 z-10 w-full  flex justify-end items-baseline">
+                <div className="flex items-center text-sm leading-6 bg-gradient-to-r from-purple-500 to-pink-500 text-white px-2 rounded-bl-md rounded-tr-md">
                     <AiOutlineIdcard className="inline-flex" size="1.5rem" />
-                    <span class="text-base ml-1">{id}</span>
+                    <span className="text-base ml-1">{id}</span>
                 </div>
             </div>
+
             <Link to={`/detail/${id}`}>
-                <img class="rounded-t-lg" src={image} alt={name} />
+                <img className="rounded-t-lg" src={image} alt={name} />
             </Link>
-            <div class="p-5">
+            <div className="p-5">
                 <Link to={`/detail/${id}`}>
-                    <h5 class="mb-2 text-2xl font-bold tracking-tight">
+                    <h5 className="mb-2 text-2xl font-bold tracking-tight">
                         {name}
                     </h5>
                 </Link>
-                <p class="flex items-center my-2 text-sm leading-6 font-normal">
+                <p className="flex items-center my-2 text-sm leading-6 font-normal">
                     <GiBackPain className="inline-flex" size="1.5rem" />
                     <span class="text-base ml-1">{species}</span>
                 </p>
-                <p class="flex items-center my-2 text-sm leading-6 font-normal">
+                <p className="flex items-center my-2 text-sm leading-6 pb-2 font-normal border-b border-solid">
                     <GiAnatomy className="inline-flex" size="1.5rem" />
                     <span class="text-base ml-1">{gender}</span>
                 </p>
-                <a
-                    href="#"
-                    class="inline-flex items-center text-sm font-medium text-center text-white bg-blue-700 rounded-lg"
-                >
-                    Read more
-                </a>
+                <div className="flex items-center mt-2 text-sm leading-6 font-normal">
+                    <button onClick={() => handleFavorite()}>
+                        {isFav ? (
+                            <AiFillHeart size="1.5rem" className="" />
+                        ) : (
+                            <AiOutlineHeart size="1.5rem" className="" />
+                        )}
+                    </button>
+                    <button onClick={() => handleClose()} className="ml-auto">
+                        <AiOutlineClose size="1.5rem" />
+                    </button>
+                </div>
             </div>
         </div>
     );
--- src/Pages/AllCards.jsx
+++ src/Pages/AllCards.jsx
@@ -5,7 +5,7 @@ import Card from "../components/Card";
 
 const characters = [
     {
-        id: 1,
+        id: 826,
         name: "Rick Sanchez",
         species: "Human",
         gender: "Male",
@@ -39,13 +39,51 @@ const characters = [
         gender: "Female",
         image: "https://rickandmortyapi.com/api/character/avatar/4.jpeg",
     },
+    {
+        id: 6,
+        name: "Rick Sanchez",
+        species: "Human",
+        gender: "Male",
+        image: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+    },
+    {
+        id: 7,
+        name: "Morty Smith",
+        species: "Human",
+        gender: "Male",
+        image: "https://rickandmortyapi.com/api/character/avatar/2.jpeg",
+    },
+    {
+        id: 8,
+        name: "Summer Smith",
+        species: "Human",
+        gender: "Female",
+        image: "https://rickandmortyapi.com/api/character/avatar/3.jpeg",
+    },
+    {
+        id: 1,
+        name: "Beth Smith",
+        species: "Human",
+        gender: "Female",
+        image: "https://rickandmortyapi.com/api/character/avatar/4.jpeg",
+    },
+    {
+        id: 9,
+        name: "Beth Smith",
+        species: "Human",
+        gender: "Female",
+        image: "https://rickandmortyapi.com/api/character/avatar/4.jpeg",
+    },
 ];
 
 const AllCards = () => {
     return (
-        <div className="max-w-screen m-5">
+        <div className="flex flex-wrap justify-center ">
             {characters.map((char) => (
-                <div key={char.id} className="my-2">
+                <div
+                    key={char.id}
+                    className="w-full sm:w-1/2 lg:w-1/3 xl:w-1/5 p-2"
+                >
                     <Card char={char} />
                 </div>
             ))}
